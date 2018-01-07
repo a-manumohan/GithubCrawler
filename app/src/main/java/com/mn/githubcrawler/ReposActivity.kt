efc0cb7f38055851f08803ebc955586c941d0a07
Manu Mohan <a.manumohan@gmail.com>
2018-01-07 12:12:29 +0400

    Add project

---
+++ app/src/main/java/com/mn/githubcrawler/ReposActivity.kt
@@ -0,0 +1,12 @@
+package com.mn.githubcrawler
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class ReposActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_repos)
+    }
+}
