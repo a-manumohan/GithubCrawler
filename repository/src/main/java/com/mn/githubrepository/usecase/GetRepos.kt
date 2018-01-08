ab5446c4aa096ebd4b802a5ef05d28f5c5c7e1ee
Manu Mohan <a.manumohan@gmail.com>
2018-01-08 11:55:27 +0400

    Refactor the get repos use case to handle local storage logic

--- repository/src/main/java/com/mn/githubrepository/usecase/GetRepos.kt
+++ repository/src/main/java/com/mn/githubrepository/usecase/GetRepos.kt
@@ -5,6 +5,8 @@ import com.mn.githubrepository.data.DataStore
 import com.mn.githubrepository.data.GithubRepo
 
 import io.reactivex.Flowable
+import org.reactivestreams.Publisher
+import org.reactivestreams.Subscriber
 
 class GetRepos(private val githubApi: GithubApi,
                private val dataStore: DataStore,
@@ -14,9 +16,22 @@ class GetRepos(private val githubApi: GithubApi,
         //todo persist locally.
         //todo if the call fails and if the page is 0, read from local store.
         return githubApi.getRepos(username, page, perPage)
+                .startWith(readFromLocalStore(page))
                 .doOnNext { repos: List<GithubRepo>? ->
                     persistToDataStore(repos)
                 }
+    }
+
+    private fun readFromLocalStore(page: Int): Publisher<List<GithubRepo>> {
+        return Publisher { s: Subscriber<in List<GithubRepo>>? ->
+            //read from local store only if the page is 0
+            if (page == 0) {
+                s?.onNext(dataStore.getItems())
+                s?.onComplete()
+            } else {
+                s?.onComplete()
+            }
+        }
     }
 
     private fun persistToDataStore(repos: List<GithubRepo>?) {
